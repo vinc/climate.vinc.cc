e6897021f5b4e440d4d86fa29599598fd0385200
Vincent Ollivier <v@vinc.cc>
2017-12-12 21:12:03 +0100

    Load dataset from url

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var build = function(data, opts) {
+  $("#chart svg").empty().removeAttr("width").removeAttr("height");
+
   var margin = { top: 40, right: 50, bottom: 40, left: 50 };
   var width = $("#chart").width() - margin.left - margin.right;
   var height = $("#chart").height() - margin.top - margin.bottom;
@@ -125,6 +127,11 @@ var build = function(data, opts) {
 var rebuildTimeout;
 var load = function(url, opts) {
   $("#chart svg").empty().removeAttr("width").removeAttr("height");
+
+  var title = "Climate Chart - " + opts.title;
+  window.history.replaceState({}, title, "?chart=" + url);
+  document.title = title;
+
   d3.csv(url, function(error, data) {
     if (error) {
       throw error;
@@ -169,9 +176,9 @@ var load = function(url, opts) {
 var menu = new Vue({
   el: "#menu",
   data: {
-    selected: "data/edc_temp.csv",
+    selected: "/data/edc_temp.csv",
     datasets: {
-      "data/greenland_d18o.csv": {
+      "/data/greenland_d18o.csv": {
         title: "Ice Core δ18O (Greenland)",
         description: "Greenland Ice Core Chronology 2005 (GICC05) 60,000 Year, 20 Year Resolution Released 10 September 2007, with d18O data from NGRIP on this time scale. NGRIP dating group, 2008.",
         source: "ftp://ftp.ncdc.noaa.gov/pub/data/paleo/icecore/greenland/summit/ngrip/gicc05-60ka-20yr.txt",
@@ -179,7 +186,7 @@ var menu = new Vue({
         x: "age",
         y: "d18o"
       },
-      "data/edc_d18o.csv": {
+      "/data/edc_d18o.csv": {
         title: "Ice Core δ18O (EPICA Dome C, Antarctica)",
         description: "EPICA Dome C Stable Isotope Data to 44.8 KYrBP. Stenni, B., et al. 2006.",
         source: "ftp://ftp.ncdc.noaa.gov/pub/data/paleo/icecore/antarctica/epica_domec/edc96-iso-45kyr.txt",
@@ -187,7 +194,7 @@ var menu = new Vue({
         x: "age",
         y: "d18o"
       },
-      "data/edc_temp.csv": {
+      "/data/edc_temp.csv": {
         title: "Ice Core Temperature (EPICA Dome C, Antarctica)",
         description: "EPICA Dome C Ice Core 800KYr Deuterium Data and Temperature Estimates. Jouzel, J., et al. 2007.",
         source: "ftp://ftp.ncdc.noaa.gov/pub/data/paleo/icecore/antarctica/epica_domec/edc3deuttemp2007.txt",
@@ -196,7 +203,7 @@ var menu = new Vue({
         x: "age",
         y: "temperature"
       },
-      "data/fuji_temp.csv": {
+      "/data/fuji_temp.csv": {
         title: "Ice Core Temperature (Dome Fuji, Antarctica)",
         description: "Dome Fuji 360KYr Stable Isotope Data and Temperature Reconstruction. Uemura, R., V. Masson-Delmotte, J. Jouzel, A. Landais, H. Motoyama, and B. Stenni. 2012.",
         source: "https://www1.ncdc.noaa.gov/pub/data/paleo/icecore/antarctica/domefuji/df2012isotope-temperature.txt",
@@ -205,7 +212,7 @@ var menu = new Vue({
         x: "age",
         y: "Tsite"
       },
-      "data/antarctic_co2.csv": {
+      "/data/antarctic_co2.csv": {
         title: "Ice Core CO2 (Antarctica)",
         description: "Antarctic Ice Cores Revised 800KYr CO2 Data. Bereiter, B.; Eggleston, S.; Schmitt, J.; Nehrbass-Ahles, C.; Stocker, T.F.; Fischer, H.; Kipfstuhl, S.; Chappellaz, J.",
         source: "https://www1.ncdc.noaa.gov/pub/data/paleo/icecore/antarctica/antarctica2015co2composite.txt",
@@ -215,28 +222,28 @@ var menu = new Vue({
         x: "age",
         y: "co2"
       },
-      "data/ice_mlo_spo_co2.csv": {
+      "/data/ice_mlo_spo_co2.csv": {
         title: "Ice Core + Atmospheric CO2",
         description: "Atmospheric CO2 record based on ice core data before 1958, (Ethridge et. al., 1996; MacFarling Meure et al., 2006) and yearly averages of direct observations from Mauna Loa and the South Pole after and including 1958 (from Scripps CO2 Program).",
         source: "http://scrippsco2.ucsd.edu/data/atmospheric_co2/icecore_merged_products",
         x: "year",
         y: "co2"
       },
-      "data/mlo_co2.csv": {
+      "/data/mlo_co2.csv": {
         title: "Atmospheric CO2 (Mauna Loa, Hawaii)",
         description: "C. D. Keeling, S. C. Piper, R. B. Bacastow, M. Wahlen, T. P. Whorf, M. Heimann, and H. A. Meijer, Exchanges of atmospheric CO2 and 13CO2 with the terrestrial biosphere and oceans from 1978 to 2000. I. Global aspects, SIO Reference Series, No. 01-06, Scripps Institution of Oceanography, San Diego, 88 pages, 2001.",
         source: "http://scrippsco2.ucsd.edu/data/atmospheric_co2/primary_mlo_co2_record",
         x: "date",
         y: "co2"
       },
-      "data/deboer2014.csv": {
+      "/data/deboer2014.csv": {
         title: "Reconstructed Sea Level",
         description: "Global 5 Million Year Sea Level, Temperature, and d18Osw Reconstructions. de Boer, B; Lourens, L.J.; van de Wal, R.S.W.",
         source: "https://www1.ncdc.noaa.gov/pub/data/paleo/reconstructions/deboer2014/deboer2014.txt",
         x: "age_calkaBP",
         y: "sealev"
       },
-      "data/deboer2014.csv?temp": {
+      "/data/deboer2014.csv?temp": {
         title: "Reconstructed Temperature",
         description: "Global 5 Million Year Sea Level, Temperature, and d18Osw Reconstructions. de Boer, B; Lourens, L.J.; van de Wal, R.S.W.",
         source: "https://www1.ncdc.noaa.gov/pub/data/paleo/reconstructions/deboer2014/deboer2014.txt",
@@ -246,12 +253,13 @@ var menu = new Vue({
     }
   },
   mounted: function() {
-    var key = this.selected;
-    load(key, this.datasets[key]);
+    var url = new URI(window.location).query(true).chart || this.selected;
+    this.selected = url;
+    load(url, this.datasets[url]);
   },
   watch: {
-    selected: function(key) {
-      load(key, this.datasets[key]);
+    selected: function(url) {
+      load(url, this.datasets[url]);
     }
   }
 });
